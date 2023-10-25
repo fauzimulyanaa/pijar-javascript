8cd67917685618d9bdb55066eac27bfd63d3baac
Fauzi Mulyana <94151536+fauzimulyanaa@users.noreply.github.com>
2023-10-25 10:37:34 +0700

    Update tugas-1.js

--- tugas-js-3/tugas-1.js
+++ tugas-js-3/tugas-1.js
@@ -14,25 +14,26 @@ const hariKerja = (day) => {
   });
 };
 
-// hariKerja("sabtu")
-//   .then((result) => {
-//     console.log(result);
-//     //digunakan untuk menetapkan tindakan yang akan diambil ketika Promise selesai (resolved) dengan sukses
-//   })
-//   .catch((err) => {
-//     console.log(err);
-//     //digunakan untuk menangani pengecualian yang terjadi selama eksekusi Promise, seperti kesalahan jaringan atau kesalahan
-//   });
+hariKerja("senin")
+  .then((result) => {
+    console.log(result);
+    //digunakan untuk menetapkan tindakan yang akan diambil ketika Promise selesai (resolved) dengan sukses
+  })
+  .catch((err) => {
+    console.log(err.message);
+    //digunakan untuk menangani pengecualian yang terjadi selama eksekusi Promise, seperti kesalahan jaringan atau kesalahan
+  });
 
-async function cekHari() {
+async function cekHari(day) {
   try {
-    const result = await hariKerja("selasa");
+    const result = await hariKerja(day);
     console.log(result);
     //biasanya kita sisipkan code javascript yang mungkin terjadi error
   } catch (error) {
-    console.log(error);
+    console.log(error.message);
     //pada blok inilah yang akan menangkap error yang terjadi pada blok Try apabila pada blok Try  error muncul
   }
 }
 
-cekHari();
+cekHari("sabtu");
+
